48ad8ad459ce0d2199ad311a7030f7c3d36dd117
Francis Y. Yan <fyy@cs.stanford.edu>
2017-09-07 11:24:13 -0700

    generate_poisson_trace.py: add duration of cold start

--- replication_with_emulation/generate_poisson_trace.py
+++ replication_with_emulation/generate_poisson_trace.py
@@ -13,6 +13,7 @@ def main():
                         help='rate of Poisson process (Mbps)')
     parser.add_argument('--output-dir', metavar='DIR', required=True,
                         help='directory to output trace')
+    parser.add_argument('--cold-start', help='seconds of cold start')
     args = parser.parse_args()
 
     # trace path
@@ -25,9 +26,11 @@ def main():
     # write timestamps to trace
     ts = 0
     with open(trace_path, 'w') as trace:
-        for i in xrange(50):
-            ts += 100
-            trace.write('%d\n' % ts)
+        if args.cold_start is not None:
+            cold_start_duration = int(float(args.cold_start) * 10)
+            for i in xrange(cold_start_duration):
+                ts += 100
+                trace.write('%d\n' % ts)
         while ts < 60000:
             ts += 1000 * random.expovariate(lambd)
             trace.write('%d\n' % ts)
